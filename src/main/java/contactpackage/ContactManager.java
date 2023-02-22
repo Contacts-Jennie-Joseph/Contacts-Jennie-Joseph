aa26cf59264d9fb84ec7e452d65c0f86317b7719
Jennie Smith <Jsmith9spiral@gmail.com>
2023-02-22 14:34:43 -0600

    update

--- src/main/java/contactpackage/ContactManager.java
+++ src/main/java/contactpackage/ContactManager.java
@@ -35,17 +35,13 @@ public class ContactManager {
             System.out.println("File write exception: " + e.getMessage());
             e.printStackTrace();
         }
-
-
-
+        mainMenu();
 
 // Load all of the contacts by calling a method that returns a List of contactpackage.Contact objects.
 // Call a method that shows the user the main menu and returns their choice of action.
 // Using the user's choice from the previous step, call a method that performs that action (modifying the contents of the List of contactpackage.Contact objects if applicable).
 // Keep calling the method from step two until the user chooses to exit.
 // Once the user chooses to exit, re-write the contents of the contacts.txt file using the List of contactpackage.Contact objects.
-
-
 
     }
 
@@ -54,7 +50,7 @@ public class ContactManager {
         while(true) {
             // print menu
             printMenu();
-            MenuChoice choice = MenuChoice.fromInt(input.getInt(0, 5, "Enter your choice: "));
+            MenuChoice choice = MenuChoice.fromInt(input.getInt(1, 5, "Enter your choice: "));
             // process user's choice
             doChoice(choice);
             // if user quits then break
@@ -67,11 +63,15 @@ public class ContactManager {
 
     private static void doChoice(MenuChoice choice) {
         switch (choice) {
-            case SearchContacts -> searchContacts();
             case ViewContacts -> viewContacts();
             case CreateContact -> theContact = (Contact) createContact();
+            case SearchContacts -> searchContacts();
             case DeleteContact -> deleteContact();
+            case Exit -> exitContact();
         }
+    }
+
+    private static void exitContact() {
     }
 
     private static void deleteContact() {
@@ -86,12 +86,19 @@ public class ContactManager {
 
     private static Object createContact() {
         String name = input.getString("Enter your contact's name: ");
-        String phoneNumber = input.getString("Enter your contact's phone number: ");
+        int phoneNumber = input.getInt("Enter your contact's phone number: ");
         Contact contact = new Contact(name, phoneNumber);
         return contact;
     }
 
     private static void printMenu() {
+        System.out.println("""
+                1. View contacts.
+                2. Add a new contact.
+                3. Search a contact by name.
+                4. Delete an existing contact.
+                5. Exit.
+                Enter an option (1, 2, 3, 4 or 5):""");
     }
 
     private static void printWelcome() {
